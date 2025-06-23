0b7f6703205cf574f43b52a17816a0a0029a70e5
0bed1ent <artemlgorevich2020@gmail.com>
2025-06-23 12:47:48 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
+
+bot = Bot(token="7079198985:AAEs67bepkXx9gyMVRXVeyGhR-e2wXNTuhs")
+dp = Dispatcher()
+
+@dp.message(Command(commands=["start"]))
+async def start(message: types.Message):
+    await message.answer("привет ты играешь в стендов? Всё равно! попробуй НОВУЮ игру!")
+dp.run_polling(bot)
